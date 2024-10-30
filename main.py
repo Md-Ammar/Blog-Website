4baa386848559874b12c70666034865b9cd63155
Md Ammar <ammaratif559@gmail.com>
2024-10-30 16:41:27 +0530

    database integrated

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:mohammar435acc@localhost/grand vlogs'
+db = SQLAlchemy(app)
+
+class Contacts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique = False, nullable=False)
+    phone_num = db.Column(db.String(12), unique = True, nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
+
 
 @app.route("/")
 def home():
@@ -10,8 +22,17 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
+@app.route("/contact", methods = ['GET', 'POST'])
 def contact():
+    if request.method=="POST":
+        '''Add entry to the database'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+        phone = request.form.get('phone')
+        entry = Contacts(name=name, phone_num=phone, email=email, msg=message)
+        db.session.add(entry)
+        db.session.commit()
     return render_template("contact.html")
 
 app.run(debug=True)
